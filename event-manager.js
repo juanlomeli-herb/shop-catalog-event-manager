2d90c52da5ec9340f893cbecbdb5d5e5b64a022d
juanlomeli-herb <juanlo@herbalife.com>
2026-02-17 11:08:37 -0600

    Add files via upload

--- event-manager.js
+++ event-manager.js
@@ -10,4 +10,31 @@
 
     }, true); // 🔥 IMPORTANTE: true = capture phase
 
+
+    function waitForProducts(){
+
+        var items = document.querySelectorAll("#subcategory .item");
+
+        if (items.length > 0) {
+
+            if (typeof coveoua === "function") {
+
+                console.log("SEARCH RESULTS READY:", items.length);
+
+                coveoua('send', 'event', 'Search', 'SearchPageLoad', {
+                    totalResults: items.length,
+                    originLevel1: "ds_en_us_myhl_search_qa01",
+                    originLevel2: "search",
+                    originLevel3: window.location.href
+                });
+
+            }
+
+        } else {
+            setTimeout(waitForProducts, 200);
+        }
+    }
+
+    waitForProducts();
+
 })();
