f62ba0a1d323006dfe7db3450732fc7198c12467
juanlomeli-herb <juanlo@herbalife.com>
2026-02-17 11:23:13 -0600

    Add files via upload

--- event-manager.js
+++ event-manager.js
@@ -15,6 +15,33 @@
 
         }, true);
 
+        // Cart
+        document.addEventListener("click", function(e){
+
+            var btn = e.target.closest(".btn-add-cart");
+            if (!btn) return;
+
+            var item = btn.closest(".item");
+            if (!item) return;
+
+            var name = item.querySelector(".name")?.innerText;
+            var sku = item.querySelector(".sku")?.innerText?.replace("SKU ", "");
+
+            console.log("ADD TO CART CLICK:", name, sku);
+
+            if (typeof coveoua === "function") {
+                coveoua('send', 'event', 'Search', 'AddToCart', {
+                    productName: name,
+                    productSku: sku,
+                    originLevel1: "ds_en_us_myhl_search_qa01",
+                    originLevel2: "search",
+                    originLevel3: window.location.href
+                });
+            }
+
+        }, true);
+
+
         // ---- OBSERVER ----
         let searchSent = false;
 
